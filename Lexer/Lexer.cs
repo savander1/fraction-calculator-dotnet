aacd2fa141f5b5528194e268cedfbae49302ec7b
Shane Vander Meer <me@shanevandermeer.com>
2018-11-03 22:20:00 -0700

    Calling it a night

--- Lexer/Lexer.cs
+++ Lexer/Lexer.cs
@@ -16,6 +16,38 @@ namespace fraction_calculator_dotnet
             var commands = builder.ToString().Split(new [] {' ', '\t', '\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
 
             
+        } 
+
+        private class ScanResult
+        {
+            public Fraction[] Fractions {get; private set;}
+            public Op[] Operations {get;private set;}
+
+            public ScanResult(Fraction[] fractions, Op[] ops )
+            {
+                Fractions = fractions;
+                Operations = ops;
+            }
+
+            public string Errors {get; set;}
+
+        }
+
+        private enum Op
+        {
+            Add,
+            Sub,
+            Mul,
+            Div
+        };
+
+        static class CommandExtensions
+        {
+            static ScanResult Scan(this string[] args)
+            {
+                throw new NotImplementedException();
+
+            }
         }
     }
 }
